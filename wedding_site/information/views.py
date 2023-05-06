dabc591c7be44f219ce568779f769ea186444767
Jan Juracka <janjuracka861@seznam.cz>
2023-05-06 12:17:59 +0200

    FIX: Info typos

--- wedding_site/information/views.py
+++ wedding_site/information/views.py
@@ -34,31 +34,31 @@ width="90%" height="40%" style="border:0;" allowfullscreen="" loading="lazy" ref
         Před sálem se bohužel parkovat nedá. Nicméně není problém na daném místě vystoupit z auta nebo z taxíku. 
         Hosté cestující z dálných končin budou moci auta nechat před hotelem, kde budou ubytovaní a v případě potřeby dojet k sálu taxíkem (nebojte se, je to kousek).''' ),
         Information('Uff schody ...', '''No, i na to jsme mysleli. <br>
-Záchody, obřadní místo i sál s hostinou jsou na přízemí a schody tak prakticky nestřetnete.'''),
-        Information('Když je potřeba trochu klidu', '''Čas od času si každý potřebuje odpočinout. Nebudeme to brát, jako neslušnost. 
+Záchody, obřadní místo i sál s hostinou jsou v přízemí a se schody se tak prakticky nestřetnete.'''),
+        Information('Když je potřeba trochu klidu', '''Čas od času si každý potřebuje odpočinout. Nebudeme to brát jako neslušnost. 
         Pro tyto situace poskytuje areál se sálem i krytou venkovní terasu, kde si budete moci užít ticho, oddechnout si a načerpat síly na další část večera.'''),
-        Information('Jak se dostat dovnitř', '''Dveře do areálu nejsou hned u ulice. Je potřeba vejít do pasáže a až tam po pravé straně najdete co hledáte.<br>
+        Information('Jak se dostat dovnitř', '''Dveře do areálu nejsou hned z ulice. Je potřeba vejít do pasáže a až tam po pravé straně najdete co hledáte.<br>
 Zdá se Vám to moc komplikované? Nebojte, kdybyste se náhodou chtěli ztratit, odchytnou Vás u dveří rodiče nevěsty.'''),
         Information('Kam si sednout', '''Zasedací pořádek si můžete prohlédnout na <a href="/guests#seats">stránce s hosty</a>. 
         Pokud se nemůžete najít, nepanikařte, zasedací pořádek i seznam hostů průběžně aktualizujeme a brzy Vás přidáme.'''),
-        Information('Co jsem sem neměl podle Nasti dávat?', 'Vedle záchodů je gauč. To je fajn, protože když náhodou bude na záchod fronta, máte si kam sednout :D.'),
+        Information('Co jsem sem neměl podle Nasti dávat?', 'Vedle záchodů je gauč. To je fajn, protože když náhodou bude na záchod fronta, máte si kam sednout \U0001F606.'),
     ]
 
     infos['Máme dovoleno fotit?'] = [
         Information(None, '''Focení vám samozřejmě zakázat nemůžeme. Máme však domluveného šikovného fotografa, co bude celý večer dokumentovat,
-        fotit párové a skupinové fotky a vlastně cokoliv co si vymyslíte. Tyto fotografie Vám v nejbližším možném termínu zveřejníme na webových stránkách.
+        fotit párové a skupinové fotky a vlastně cokoliv, co si vymyslíte. Tyto fotografie Vám v nejbližším možném termínu zveřejníme na webových stránkách.
         Budeme tedy rádi, když necháte vaše mazlíčky s bleskem doma, aby se jim nic nestalo a abyste na ně nemuseli dávat pozor.
         ''')
     ]
 
     infos['Pozornosti'] = [
         Information('Květy', '''Plánujete přinést obrovskou honosnou kytici? A už někdy někdo z Vás musel přenášet z 
-        Trnavy do Brna vlakem těch kytic několik a doufat, že se to vejde do malého bytíku? <br>
+        Trnavy do Brna vlakem těch kytic několik a doufat, že se to vejde do malého bytečku? <br>
 V této situaci bychom se mohli ocitnout i my a věříme, že toto nikdo nikomu nepřeje. Naopak nás potěší, když jako 
 pozornost donese každý jednu růži. Všechny růže se potom budou umísťovat do velké vázy a tak vznike jedna
  obrovská společná kytice, kterou si můžeme s láskou v srdci odnést.'''),
-        Information('Svatební dary', '''Lámete si hlavu s tím, co nám vybrat? Nebo naopak už kujete pykle a snažíte se nám připravit obrovské překvapení? <br>
-Bohužel budeme ještě řádku let bývat v malém bytě, který už nové věci nepojme a transport dárků společně s ostatními věci ze svatby by byl přinejmenším komplikovaný. 
+        Information('Svatební dary', '''Lámete si hlavu s tím, co nám vybrat? Nebo naopak už kujete pi  kle a snažíte se nám připravit obrovské překvapení? <br>
+Bohužel budeme ještě řádku let bývat v malém bytě, který už nové věci nepojme a transport dárků společně s ostatními věcmi ze svatby by byl přinejmenším komplikovaný. 
 To nejdůležitější už přece máme, našli jsme si sami sebe. <br> Pokud byste nás i přesto chtěli obdarovat, budeme rádi za jakýkoliv finanční příspěvek na naši vysněnou svatební cestu.''')
     ]
 
